ac4df64132b8d4d826c8fa579b57cd7c95650656
jiajia77 <2604425169@qq.com>
2016-10-10 16:32:37 +0800

    侧滑菜单样式，及小红点数字提醒

--- app/src/main/java/com/aritime/aridj/base/BaseActivity.java
+++ app/src/main/java/com/aritime/aridj/base/BaseActivity.java
@@ -1,14 +1,6 @@
 package com.aritime.aridj.base;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
-
-import com.aritime.aridj.R;
-import com.jaeger.library.StatusBarUtil;
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * Created by jiajia on 2016/10/8.
--- app/src/main/java/com/aritime/aridj/main/MainActivity.java
+++ app/src/main/java/com/aritime/aridj/main/MainActivity.java
@@ -1,14 +1,28 @@
 package com.aritime.aridj.main;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.aritime.aridj.R;
 import com.aritime.aridj.base.BaseActivity;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.jaeger.library.StatusBarUtil;
 
 /**
@@ -20,7 +34,13 @@ public class MainActivity extends BaseActivity {
     private Toolbar mToolbar;
     private NavigationView mNavView;
 
-    private int mStatusBarColor;//状态栏颜色
+    private int mStatusBarColor;// 状态栏颜色
+    private boolean flag_drawer = false;// 抽屉打开状态标志位，true：打开，false：关闭
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +48,9 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.act_main);
         initView();
         initEvent();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void initView() {
@@ -46,14 +69,43 @@ public class MainActivity extends BaseActivity {
 
         // 设置menu图标显示原始颜色
         mNavView.setItemIconTintList(null);
+
+        //TODO 提示角标
+        LinearLayout ll = (LinearLayout)mNavView.getMenu().findItem(R.id.drawer_data_manage).getActionView();
+        TextView tv= (TextView) ll.findViewById(R.id.tv_tint_msg);
+        tv.setText("7");
+       /* Button tintmsg = (Button) MenuItemCompat.getActionView(mNavView.getMenu().findItem(R.id.drawer_data_updating));
+        tintmsg.setWidth(5);
+        tintmsg.setHeight(5);
+        tintmsg.setPadding(60,8,10,8);
+        tintmsg.setBackgroundResource(R.mipmap.icon_prompt);
+        tintmsg.setGravity(Gravity.CENTER_VERTICAL);
+        tintmsg.setTextColor(Color.WHITE);
+        tintmsg.setText("强制");*/
     }
 
     private void initEvent() {
 
         /* 打开关闭侧滑菜单事件监听，及状态同步 */
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.opendrawer,
-                R.string.closedrawer);
-        mDrawerLayout.setDrawerListener(toggle);
+                R.string.closedrawer) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                flag_drawer = false;
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                flag_drawer = true;
+            }
+
+         /*   @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                flag_drawer=false;
+                return super.onOptionsItemSelected(item);
+            }*/
+        };
+        mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
 
@@ -64,5 +116,54 @@ public class MainActivity extends BaseActivity {
         /* 设置DrawerLayout沉浸式菜单栏样式 */
         mStatusBarColor = getResources().getColor(R.color.colorPrimary);
         StatusBarUtil.setColorForDrawerLayout(MainActivity.this, mDrawerLayout, mStatusBarColor, 0);
+    }
+
+    /**
+     * 模拟返回键
+     */
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        /*若侧滑菜单处于打开状态，关闭*/
+        if (flag_drawer) {
+            mDrawerLayout.closeDrawers();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
