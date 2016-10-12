c1aa8ba0f37d8e43cada6f19fa6f31081409b8c4
jiajia77 <2604425169@qq.com>
2016-10-12 16:43:58 +0800

    TabLayout

--- app/src/main/java/com/aritime/aridj/main/MainActivity.java
+++ app/src/main/java/com/aritime/aridj/main/MainActivity.java
@@ -1,29 +1,22 @@
 package com.aritime.aridj.main;
 
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.MenuItemCompat;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.KeyEvent;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.aritime.aridj.R;
 import com.aritime.aridj.base.BaseActivity;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.jaeger.library.StatusBarUtil;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 /**
  * Created by jiajia on 2016/10/8.
@@ -33,14 +26,12 @@ public class MainActivity extends BaseActivity {
     private DrawerLayout mDrawerLayout;
     private Toolbar mToolbar;
     private NavigationView mNavView;
+    private ViewPager mViewPager;
+//    private SmartTabLayout mTabLayout;
+    private TabLayout mTabLayout;
 
     private int mStatusBarColor;// 状态栏颜色
     private boolean flag_drawer = false;// 抽屉打开状态标志位，true：打开，false：关闭
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,15 +39,14 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.act_main);
         initView();
         initEvent();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void initView() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mNavView = (NavigationView) findViewById(R.id.main_nav);
+        mViewPager = (ViewPager) findViewById(R.id.vp_mian);
+        mTabLayout =(TabLayout)findViewById(R.id.tabl_maian);
 
         /* 设置toolbar不显示标题、显示背景色及logo */
         setSupportActionBar(mToolbar);
@@ -71,17 +61,12 @@ public class MainActivity extends BaseActivity {
         mNavView.setItemIconTintList(null);
 
         //TODO 提示角标
-        LinearLayout ll = (LinearLayout)mNavView.getMenu().findItem(R.id.drawer_data_manage).getActionView();
-        TextView tv= (TextView) ll.findViewById(R.id.tv_tint_msg);
-        tv.setText("7");
-       /* Button tintmsg = (Button) MenuItemCompat.getActionView(mNavView.getMenu().findItem(R.id.drawer_data_updating));
-        tintmsg.setWidth(5);
-        tintmsg.setHeight(5);
-        tintmsg.setPadding(60,8,10,8);
-        tintmsg.setBackgroundResource(R.mipmap.icon_prompt);
-        tintmsg.setGravity(Gravity.CENTER_VERTICAL);
-        tintmsg.setTextColor(Color.WHITE);
-        tintmsg.setText("强制");*/
+        LinearLayout ll = (LinearLayout) mNavView.getMenu().findItem(R.id.drawer_data_manage).getActionView();
+        TextView tv = (TextView) ll.findViewById(R.id.tv_tint_msg);
+        tv.setText("67");
+
+
+
     }
 
     private void initEvent() {
@@ -99,14 +84,15 @@ public class MainActivity extends BaseActivity {
                 flag_drawer = true;
             }
 
-         /*   @Override
-            public boolean onOptionsItemSelected(MenuItem item) {
-                flag_drawer=false;
-                return super.onOptionsItemSelected(item);
-            }*/
         };
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        //标签页的设置
+        MyFragmentPagerAdapter fragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(),this);
+        mViewPager.setAdapter(fragmentPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+
 
 
     }
@@ -129,41 +115,5 @@ public class MainActivity extends BaseActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
